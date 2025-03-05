aa1a3a1243ca7411ccda5d7abcdfcb4beea2d95e
msagolj <msagolj@adobe.com>
2025-03-05 15:32:15 +0100

    add search template

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -27,7 +27,8 @@ function updateUEInstrumentation() {
   const main = document.querySelector('main');
   const template = document.querySelector('meta[name="template"]')?.content;
   const sections = main.querySelectorAll('[data-aue-model="section"]');
-  const templates = ['order-details', 'enrichment', 'pdp', 'cart', 'mini-cart', 'plp', 'checkout', 'search-order'];
+add  const templates = ['order-details', 'enrichment', 'pdp', 'cart', 'mini-cart', 'plp', 
+    'checkout', 'search-order', 'search'];
 
   // updated section filters according to the template
   if (templates.includes(template)) {
